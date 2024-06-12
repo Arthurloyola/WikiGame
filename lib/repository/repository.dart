91f6bc069528136ae1647628deccb2e3e545f9e9
Arthur Loyola <arthurloyolasoares@gmail.com>
2024-06-11 23:14:52 -0300

    Reparando erros, e chamando o style na main

--- lib/repository/repository.dart
+++ lib/repository/repository.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-
 import 'package:wikigame/model/game_response.dart';
 import 'package:http/http.dart' as http;
 
@@ -9,18 +8,20 @@ class GameRepository {
   final String apiKey = '<Here is your api key>';
 
   Future<GameResponse> getGames() async {
-    var response = await http.post(gameListUrl,
-        headers: {"Acceppt": "application/json", "user-key": "$apiKey"},
-        body: "");
+    var response = await http.post(Uri.parse(gameListUrl),
+        headers: {"Accept": "application/json", "user-key": "$apiKey"},
+        body:
+            "fields artworks,bundles,category,checksum,collection,cover.*,created_at,first_release_date,follows,game_engines.*,game_modes.*,release_dates,genres.*,hypes,keywords.*,multiplayer_modes,name,parent_game,platforms.*,platforms.platform_logo,player_perspectives.*,popularity,rating,rating_count,screenshots.*,slug,standalone_expansions,status,storyline,summary,tags,time_to_beat,total_rating,total_rating_count,updated_at,url,version_parent,version_title,videos.*; where cover.image_id != null & genres != null & videos != null & created_at > 1252214987 & platforms = 48 & rating > 80; limit 100; sort popularity desc;");
 
     print("${response.statusCode}");
     return GameResponse.fromJson(jsonDecode(response.body));
   }
 
   Future<GameResponse> getSlider() async {
-    var response = await http.post(gameListUrl,
-        headers: {"Acceppt": "application/json", "user-key": "$apiKey"},
-        body: "");
+    var response = await http.post(Uri.parse(gameListUrl),
+        headers: {"Accept": "application/json", "user-key": "$apiKey"},
+        body:
+            "fields artworks,bundles,category,checksum,collection,cover.*,created_at_first_release_date, follows_game_engines.*,game_modes.*,release_dates,genres.*,hypes,keywords.*,multiplayer_modes,name,parent_game,platforms.*,platforms.platform_logo,player_perspectives.*popularity,rating,rating_count,screenshots.*,slug,standalone_expansions,status,storyline,summary,tags,time_to_beat,total_rating,total_rating_count,updated_at, url,version_parent version_title_videos.*;where cover.image_id != null & genres != null & vídeos != null & created at > 1252214987 & platforms = 48;limit 100; sort created_at asc;");
     print("${response.statusCode}");
     return GameResponse.fromJson(jsonDecode(response.body));
   }
